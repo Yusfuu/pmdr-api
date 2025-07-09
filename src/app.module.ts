f0d072c019e4621fc987d6b2c3f77e16007218f4
Yusfuu <youssefhajjari01@gmail.com>
2025-07-09 16:40:28 +0100

    inti

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,36 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
-import { UsersModule } from './users/users.module';
-import { RoomsModule } from './rooms/rooms.module';
-import { AuthModule } from './auth/auth.module';
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      typePaths: ['./**/*.graphql', './**/*.gql'],
-      path: '/gql',
-      definitions: {
-        path: join(process.cwd(), 'src/graphql.ts'),
-        outputAs: 'interface',
-      },
-      subscriptions: {
-        'graphql-ws': true,
-      },
-      context: ({ req, res }) => ({ req, res }),
-      plugins: [ApolloServerPluginLandingPageLocalDefault()],
-      playground: false,
-    }),
-    UsersModule,
-    RoomsModule,
-    AuthModule,
-  ],
-  controllers: [],
-  providers: [AppService],
-  exports: [],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
